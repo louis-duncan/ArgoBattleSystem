2793a8b7a90089e3dc212816f498bcf397f56a1b
Louis <louisthurman@gmail.com>
2019-04-08 16:14:50 +0100

    Setting up object updating.

--- client_side.py
+++ client_side.py
@@ -1,3 +1,4 @@
+import time
 import easygui
 import pygame
 
@@ -12,6 +13,15 @@ FONT = "fonts/newyorkescape.ttf"
 # FONT = "fonts/CHECKBK0.TTF"
 # FONT = "fonts/Beef'd.ttf"
 SWEEP_SPRITE = pygame.image.load("sprites/sweep.png")
+REFRESH_RATE = 1
+SPRITES_DIR = "sprites/"
+sprite_defs = {"trail": "cs_trail.png",
+               "ship": "cs_ship.png",
+               "?": "cs_unknown.png"}
+sprites = {}
+for k in sprite_defs:
+    sprites[k] = pygame.image.load(os.path.join(SPRITES_DIR, sprite_defs[k]))
+OBJECT_SPRITES = sprites
 
 
 class Display:
@@ -26,9 +36,11 @@ class Display:
         self.font = pygame.font.Font(FONT, round(self.cell_size * 0.5), bold=1)
         sweep_rect = (self.board_pos[0] + self.cell_size, self.board_pos[1] + self.cell_size,
                       (self.cell_size * self.grid_size[0]), (self.cell_size * self.grid_size[1]))
-        self._sweep = Sweep(sweep_rect, round(self.screen_size[1] / 40), cell_size=self.cell_size)
+        self._sweep = Sweep(sweep_rect, self.cell_size)
         self._last_data_read = None
         self._getter = HTTPGetter()
+        self._last_refresh_time = 0
+        self._last_updated_col = -1
 
     def add_entities(self, entities):
         pass
@@ -74,11 +86,31 @@ class Display:
                 screen.blit(char_render, char_pos)
 
     def update(self):
-        self._check_for_updates()
+        if self._last_refresh_time - time.time() > REFRESH_RATE and self._sweep.finished:
+            self._check_for_updates()
+            self._last_updated_col = -1
+
+        if not self._sweep.finished:
+            self._sweep.update()
+            col_to_refresh = (self._sweep.x_pos // self.cell_size) - 1
+            if col_to_refresh >= 0 and col_to_refresh != self._last_updated_col:
+                print("Updating col", col_to_refresh)
+
+                # Todo: Add refresh code.
+
+                self._last_updated_col = col_to_refresh
+
+
 
         for o in self.objects:
             o.update()
-        self._sweep.update()
+
+    def get_object(self, pos):
+        for o in self.objects:
+            if o.pos == pos:
+                return o
+            else:
+                pass
 
     def draw(self, screen):
         self._draw_board(screen)
@@ -86,7 +118,7 @@ class Display:
             o.draw(screen)
         self._sweep.draw(screen)
 
-    def sweep(self):
+    def start_sweep(self):
         self._sweep.reset()
 
     def _check_for_updates(self):
@@ -102,29 +134,38 @@ class Display:
 
 
 class ScreenObject:
-    def __init__(self, pos, type_text, direction):
-        self.pos = pos
+    def __init__(self, grid_pos, real_pos, type_text, direction):
+        self.grid_pos = grid_pos
+        self.real_pos = real_pos
         self.type_text = type_text
         self.direction = direction
+        self.fade_out = False
+        self.gone = False
 
     def matches(self, obj):
-        return self.pos == obj.pos and self.type_text == obj.type_text and self.direction == obj.direction
+        return self.grid_pos == obj.grid_pos and self.type_text == obj.type_text and self.direction == obj.direction
+
+    def update(self):
+        pass
+
+    def draw(self, screen):
+        pass
 
 
 class Sweep:
-    def __init__(self, rect, speed=20, cell_size=20):
+    def __init__(self, rect, cell_size=20):
         self._rect = rect
-        self._speed = speed
+        self._speed = cell_size - 1
         self._sprite_width = 4 * cell_size
         self._sprite = pygame.transform.scale(SWEEP_SPRITE, (self._sprite_width, self._rect[3]))
-        self._x_pos = -self._sprite_width
+        self.x_pos = -self._sprite_width
         self.finished = False
 
     def update(self):
         if self.finished:
             return
-        self._x_pos += self._speed
-        if self._x_pos > self._rect[2]:
+        self.x_pos += self._speed
+        if self.x_pos > self._rect[2]:
             self.finished = True
 
     def draw(self, screen):
@@ -136,21 +177,21 @@ class Sweep:
         #                   self._rect[1]) + (self._sprite.get_size()[0],
         #                                     self._sprite.get_size()[1]))
 
-        if self._x_pos >= 0 and self._x_pos + self._sprite_width <= self._rect[2]:
-            screen.blit(self._sprite, (self._rect[0] + self._x_pos, self._rect[1]))
-        elif self._x_pos > self._rect[2] - self._sprite_width:
-            screen.blit(self._sprite, (self._rect[0] + self._x_pos, self._rect[1]),
+        if self.x_pos >= 0 and self.x_pos + self._sprite_width <= self._rect[2]:
+            screen.blit(self._sprite, (self._rect[0] + self.x_pos, self._rect[1]))
+        elif self.x_pos > self._rect[2] - self._sprite_width:
+            screen.blit(self._sprite, (self._rect[0] + self.x_pos, self._rect[1]),
                         (0,  # Crop x
                          0,  # Crop y
-                         self._sprite_width - ((self._x_pos + self._sprite_width) - self._rect[2]),  # Crop width
+                         self._sprite_width - ((self.x_pos + self._sprite_width) - self._rect[2]),  # Crop width
                          self._rect[3],  # Crop height
                          ))
-        elif self._x_pos < 0 and abs(self._x_pos) < self._sprite_width:
+        elif self.x_pos < 0 and abs(self.x_pos) < self._sprite_width:
             screen.blit(self._sprite,
                         (self._rect[0], self._rect[1]),  # Pos
-                        (- self._x_pos,  # Crop x
+                        (- self.x_pos,  # Crop x
                          0,  # Crop y
-                         self._sprite_width + self._x_pos,  # Crop width
+                         self._sprite_width + self.x_pos,  # Crop width
                          self._rect[3]  # Crop height
                          )
                         )
@@ -158,7 +199,7 @@ class Sweep:
             pass
 
     def reset(self):
-        self._x_pos = -self._sprite_width
+        self.x_pos = -self._sprite_width
         self.finished = False
 
 
@@ -229,7 +270,7 @@ def main():
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 return
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-                viewer.sweep()
+                viewer.start_sweep()
 
         viewer.update()
 
