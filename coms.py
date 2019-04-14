29670b33e6197ebd5ff049d338b0a35c5784654d
Louis <louisthurman@gmail.com>
2019-04-14 03:40:01 +0100

    FTP sender ditched, socket connection implemented

--- coms.py
+++ coms.py
@@ -1,65 +1,34 @@
-import ftplib
+import socket
 import json
-import os
-import random
 import time
 
 import requests
 import _thread
 
 HOST = "home.ltcomputing.co.uk"
-FTP_PORT = 31416
+UPLOAD_PORT = 31416
 HTTP_PORT = 31417
-USER = "ftp_client"
-PASS = "password"
+
 TEMP_DIR = "tmp/"
 
 
-class FTPSender:
+class Sender:
     def __init__(self):
-        self._ftp = ftplib.FTP()
         self._queue = []
         self.worker_pid = _thread.start_new(self._worker, ())
 
-    def _connect(self):
-        self._ftp.connect(HOST, FTP_PORT)
-        self._ftp.login(USER, PASS)
-
-    def _close(self):
-        self._ftp.quit()
-
-    def _format_for_sending(self, obj):
-        path = os.path.join(TEMP_DIR, str(random.randint(100000, 999999)) + ".json")
-        with open(path, "w") as fh:
-            fh.write(json.dumps(obj))
-        return path
-
-    def _ftp_upload(self, source, destination):
-        self._connect()
-        with open(source, "br") as fh:
-            self._ftp.storbinary("STOR {}".format(destination), fh)
-
-    def send(self, obj, ship_name):
-        self._queue.append((obj, ship_name))
+    def send(self, ship_name, obj):
+        self._queue.append((ship_name, obj))
 
     def _worker(self):
         while True:
-            if len(self._queue) > 0:
-                obj, ship_name = self._queue.pop(0)
-                path = self._format_for_sending(obj)
-                done = False
-                count = 0
-                while not done and count < 3:
-                    count += 1
-                    try:
-                        self._connect()
-                        self._ftp_upload(path, "ship_{}.json".format(ship_name))
-                        done = True
-                    except ftplib.all_errors:
-                        print("Failed to send:", obj, " Attempt no:", count)
-                time.sleep(0.5)
-                os.remove(path)
-            time.sleep(0.2)
+            for q in self._queue:
+                s = socket.socket()
+                s.connect((HOST, UPLOAD_PORT))
+                s.send(json.dumps(q).encode())
+                s.close()
+            self._queue.clear()
+            time.sleep(0.1)
 
 
 class HTTPGetter:
@@ -71,6 +40,9 @@ class HTTPGetter:
                                                             HTTP_PORT,
                                                             ship_name))
         if r.status_code == 200:
-            return json.loads(r.text)
+            try:
+                return json.loads(r.text)
+            except json.JSONDecodeError:
+                return list()
         else:
             return list()
