fd1ad3d5e47ae86ad50d18a4a2130b488ef61fcc
Louis <louisthurman@gmail.com>
2019-04-14 03:33:57 +0100

    Entire Rewrite
    Removed FTP usage, now opens sockets and writes data directly.

--- server_side.py
+++ server_side.py
@@ -1,26 +1,77 @@
 import os
+import socket
+import json
 import time
 
-WWW_DIR = "/var/www/html"
+WWW_DIR = ""
+LISTEN_PORT = ""
+with open("server_config.json", "r") as config_fh:
+    config_data = json.loads(config_fh.read())
+    assert type(config_data) is dict
+    for k in config_data:
+        locals()[k] = config_data[k]
+
+assert "" not in (WWW_DIR, LISTEN_PORT)
 
 
-def get_valid_files(path=None, prefix="ship_", suffix=".json"):
-    names = os.listdir(path)
-    valid = []
-    for n in names:
-        if n.startswith(prefix) and n.endswith(suffix):
-            valid.append(n)
-    return valid
+class NoIPError(Exception):
+    pass
 
 
-while True:
-    time.sleep(1)
-    file_names = get_valid_files()
-    for f in file_names:
-        print("Copying {} to {}...".format(f, WWW_DIR))
-        with open(f, "r") as sfh:
-            with open(os.path.join(WWW_DIR, f), "w") as dfh:
-                dfh.write(sfh.read())
-        print("Removing {}...".format(f))
-        os.remove(f)
-        print("Complete")
+def get_ip():
+    name, alias_list, address_list = socket.gethostbyname_ex(socket.gethostname())
+    for ip in address_list:
+        if ip.startswith("10.") or ip.startswith("192.168."):
+            return ip
+    raise NoIPError("No local IP address could be found.")
+
+
+def clear_all():
+    print(time.ctime(time.time()), " - Clearing:")
+    for p in os.listdir(WWW_DIR):
+        if p.startswith("ship_") and p.endswith(".json"):
+            print(" - ", os.path.join(WWW_DIR, p))
+            with open(os.path.join(WWW_DIR, p), "w") as fh:
+                fh.write(json.dumps("clear"))
+
+    time.sleep(3)
+    print(time.ctime(time.time()), " - Deleting:")
+    for p in os.listdir(WWW_DIR):
+        if p.startswith("ship_") and p.endswith(".json"):
+            print(" - ", os.path.join(WWW_DIR, p))
+            os.remove(os.path.join(WWW_DIR, p))
+
+
+def worker(s):
+    print(time.ctime(time.time()), " - Receiving from {}...".format(s.getpeername()))
+    data = ""
+    done = False
+    while not done:
+        b = s.recv(1)
+        if b == b"":
+            done = True
+        else:
+            data += b.decode()
+    ship_name, data = json.loads(data)
+    print(" - Ship:", ship_name)
+    print(" - Data:", data)
+    if ship_name == "" and data == "reset":
+        clear_all()
+        return
+    file_name = os.path.join(WWW_DIR, "ship_{}.json".format(ship_name))
+    with open(file_name, "w") as fh:
+        fh.write(json.dumps(data))
+    print(time.ctime(time.time()), " - Saved to", file_name)
+
+
+def main():
+    ss = socket.socket()
+    ss.bind((get_ip(), LISTEN_PORT))
+    ss.listen(5)
+    print(time.ctime(time.time()), " - Server started!")
+    print(time.ctime(time.time()), " - Listening on", get_ip(), "-", LISTEN_PORT)
+    while True:
+        worker(ss.accept()[0])
+
+
+main()
