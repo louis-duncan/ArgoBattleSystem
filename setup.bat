1bb2a76b52da02852378ead11d9e4a75dd5ead70
Louis <louisthurman@gmail.com>
2019-04-30 11:36:28 +0100

    Created setup bat

---
+++ setup.bat
@@ -0,0 +1,13 @@
+echo off
+cls
+echo Installing requests...
+pip install requests
+echo Done.
+echo Installing easygui...
+pip install easygui
+echo Done.
+echo Installing pygame...
+pip install pygame
+echo Done.
+Install complete!
+pause
